c9218a41bd7040df0021c63794780720a765ca91
Aleksander Shustov <krisale@yandex.ru>
2022-05-21 18:04:24 +0300

    fix: 01 - add module path

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,3 +1,5 @@
 const fs = require('fs');
-const fd = fs.createReadStream('01-read-file/text.txt', 'utf-8')
-fd.on('data', chunk => console.log(chunk))
+const path = require('path');
+const pathFile = path.join(__dirname, 'text.txt');
+const fd = fs.createReadStream(pathFile, 'utf-8');
+fd.on('data', chunk => console.log(chunk));
